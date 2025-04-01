01862828077eb65942e8cddc1dd3ab4470f43b58
Liane Sampaio <lianeester.sampaio@gmail.com>
2025-04-01 11:35:25 +0200

    up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,10 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 function Navbar() {
-    return (_jsxs("nav", { children: [_jsx("div", { className: "logo", children: "Liane Sampaio" }), _jsxs("ul", { children: [_jsx("li", { children: _jsx("a", { href: "#about", children: "About" }) }), _jsx("li", { children: _jsx("a", { href: "#projects", children: "Projects" }) }), _jsx("li", { children: _jsx("a", { href: "#contact", children: "Contact" }) })] }), _jsx("div", { className: "flex items-center justify-center bg-gray-200", children: _jsx("button", { className: "btn btn-accent", children: "Accent" }) })] }));
+    return (
+        _jsxs("nav", { children: [_jsx("div", { className: "logo", children: "Liane Sampaio" }),
+        _jsxs("ul", { children: [_jsx("li", { children: _jsx("a", { href: "#about", children: "About" }) }),
+        _jsx("li", { children: _jsx("a", { href: "#projects", children: "Projects" }) }),
+        _jsx("li", { children: _jsx("a", { href: "#contact", children: "Contact" }) })] }),
+    ] }));
 }
 export default Navbar;
